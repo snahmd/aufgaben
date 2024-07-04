84db9d881fff62e2f100b4fdd42f16de9982e487
snahmd <sanahmed7@gmail.com>
2024-07-04 18:36:52 +0200

    add js einfuhrung

---
+++ Juli4-JS_Intro/variablen/Variablen-Level-1_5/assets/js/main.js
@@ -0,0 +1,21 @@
+console.log("Hallo, Welt!!!!");
+
+let carName = "BMW";
+console.log(carName);
+
+///
+x = 150;
+y = 50;
+z = x + y;
+console.log(x);
+console.log(y);
+console.log(z);
+
+///
+let firstName = "John",
+  lastName = "Doe",
+  age = 35;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(firstName + " " + lastName + " " + age);
